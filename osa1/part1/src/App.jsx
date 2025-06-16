6d7fe3f9520a1f1a74b643ffc4b4c4528e56ce70
Jimi <jhkorte@proton.me>
2025-06-16 11:22:30 +0300

    osa1 harjoitustehtäviä

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,10 +1,36 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+  
   return (
     <div>
       <p>
-        Hei {props.name}, olet {props.age} vuotta vanha!
+        Hei {name}, olet {age} vuotta vanha!
+      </p>
+      <p>
+        Synnyit varmaankin vuonna {bornYear()}
       </p>
     </div>
+  )
+}
+
+
+const Display = (props) => {
+  return (
+    <div>
+      {props.counter}
+    </div>
+  )
+}
+
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClickAction}>
+      {props.text}
+    </button>
   )
 }
 
@@ -19,28 +45,46 @@ const App = () => {
   const nimi="Pekka"
   const ika=10
 
+  const [ counter, setCounter ] = useState(0)
+
+  const counterIncrement = () => setCounter(counter + 1)
+  const counterDecrement = () => setCounter(counter - 1)
+  const counterSetZero = () => setCounter(0)
+
+  /*
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+  */
+
   return (
-  <>
-    <h1>Greetings</h1>
-    <Hello name="Maya" age={a+b} />
+  <div>
+    <h1>Tervehdys</h1>
+    
+    <Hello name="Maya" age={26 + 10} />
     <Hello name={nimi} age={ika} />
-  </>
-  )
-}
+    <Hello name="ihme ukkeli" age={counter} />
 
-/*
-const App = () => {
-  const friends = [
-    'Leevi','Venla'
-  ]
-
-  return (
     <div>
-      <p>{friends}</p>
+      <Display counter={counter} />
+      <Button
+        onClickAction={counterIncrement} 
+        text='Counter++' 
+      />
+      <Button 
+        onClickAction={counterDecrement} 
+        text='Counter--' 
+      />
+      <Button 
+        onClickAction={counterSetZero}
+        text='Reset' 
+      />
     </div>
+
+  </div>
   )
 }
-*/
 
 
 export default App
