7e5840d51ee7ff238abd796bb4db72f788709aa4
Jimi <jhkorte@proton.me>
2025-07-13 16:20:09 +0300

    1.7 tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -25,23 +25,32 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [pos, setPos] = useState(0)
 
   const actionFeedbackGood = () => {
     console.log('good feedback')
     console.log(good)
     setGood(good +1)
+    setTotal(total +1)
+    setAverage(average +1)
   }
 
   const actionFeedbackNeutral = () => {
     console.log('neutral feedback')
     console.log(neutral)
     setNeutral(neutral +1)
+    setTotal(total +1)
+    setAverage(average +0)
   }
 
   const actionFeedbackBad = () => {
     console.log('bad feedback')
     console.log(bad)
     setBad(bad +1)
+    setTotal(total +1)
+    setAverage(average -1)
   }
 
   return ( 
@@ -55,7 +64,7 @@ const App = () => {
     <div>
       <Header header = {header2} />
       <p>
-        good {good} <br /> neutral {neutral} <br /> bad {bad}
+        good {good} <br /> neutral {neutral} <br /> bad {bad} <br /> total {total} <br /> average {average / total} <br /> pos% {(good / total)*100} %
       </p>
     </div>
   </div>
