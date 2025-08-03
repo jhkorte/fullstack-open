6e6d532ababfaf7cb6179ca5ca191e098cc26795
Jimi <jhkorte@proton.me>
2025-08-03 16:49:38 +0300

    3.3

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -43,7 +43,17 @@ app.get('/info', (request, response) => {
         ${Date()}
         </div>`
     )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
     
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 
