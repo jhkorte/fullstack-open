22077c3bc0db21ff7a1c6c3e691b1bf94ecee76e
Jimi <jhkorte@proton.me>
2025-08-04 19:39:13 +0300

    3.11 & 3.10 & 3.9 tehty

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
     return [
