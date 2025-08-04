2ae7c6bc6b7bc26ac886af472c93c8064a2cd7e4
Jimi <jhkorte@proton.me>
2025-08-04 16:34:39 +0300

    3.8

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -3,7 +3,16 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
+}))
 
 
 let persons = [
@@ -23,7 +32,7 @@ let persons = [
         number: "12-43-234345",
     },
     {
-        id: "45000",
+        id: "4",
         name: "Mary Poppendieck",
         number: "39-23-6423122",
     },
