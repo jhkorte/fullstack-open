6ece90733cfb2a4b5d1b9c965d859d3db40c43a2
Jimi <jhkorte@proton.me>
2025-08-07 16:34:47 +0300

    Tehty ominaisuudet 3.18 - 3.15

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -19,7 +19,7 @@ app.use(morgan(function (tokens, req, res) {
     ].join(' ')
 }))
 
-
+/*
 let persons = [
     {
         id: "1",
@@ -42,26 +42,32 @@ let persons = [
         number: "39-23-6423122",
     },
 ]
-
+*/
 
 app.get('/', (request, response) => {
     response.send('<h1>hello osa3</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    //response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
-    response.send(
-        `<div>
-        This phonebook has info for ${persons.length} people <br />
-        ${Date()}
-        </div>`
-    )
+    Person.find({}).then(persons => {
+        response.send(
+            `<div>
+            This phonebook has info for ${persons.length} people <br />
+            ${Date()}
+            </div>`
+        )
+    })
+    
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     /*
     const id = request.params.id
     const person = persons.find(person => person.id === id)
@@ -73,25 +79,40 @@ app.get('/api/persons/:id', (request, response) => {
     }
     */
 
+    /*
     Person.findById(request.params.id).then(person => {
         response.json(person)
     })
+    */
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
+app.delete('/api/persons/:id', (request, response, next) => {
+    /*
+    Person.find({}).then(persons => {
+        console.log('Before deleting:')
+        persons.forEach(person => console.log(person))
+    })
+    */
 
-    //Check if ID exists in persons
-    if (persons.some(person => person.id === id)) {
-        persons = persons.filter(person => person.id !== id)
-        console.log(`Deleting person id ${id}`)
-        console.log(persons)
-
-        response.status(204).end()
-    } else {
-        console.log(`No person found with id ${id}`)
-        response.status(404).end()
-    }
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            Person.find({}).then(updatedPersons => {
+                console.log(`After deleting ${request.params.id} `, updatedPersons)
+            })
+            
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -103,33 +124,84 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    /*  Re-written below to work with mongodb. This code is for the hard-coded "database". 
+        Leaving this here for now, for reference.
+
     if (persons.some(person => person.name === body.name)) {
         console.log('attempted to add non-unique person')
         return response.status(400).json({
             error: 'name must be unique'
         })
     }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
-
-    person.save().then(savedPerson => {
-        persons = persons.concat(savedPerson)
-        response.json(savedPerson)
-        console.log('added new person', savedPerson)
-        console.log(persons)
-    })
-
-    /*
-    persons = persons.concat(person)
-    response.json(person)
-
-    console.log('added new person', person)
-    console.log(persons)
     */
+
+    Person.findOne( { name: body.name }).then(existingPerson => {
+        if (existingPerson) {
+            console.log('attempted to add non-unique person')
+            return response.status(400).json({
+                error: 'name must be unique'
+            })
+        }
+
+        const person = new Person({
+           name: body.name,
+           number: body.number,
+        })
+
+
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+            console.log('added new person', savedPerson)
+            Person.find({}).then(persons => {
+                console.log('After adding new person:')
+                persons.forEach(person => console.log(person))
+            })
+        })
+    })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then((updatedPerson) => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+})
+
+
+// ---------------------- No routes beyond this line ----------------------
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// This must be called only after all other middlewares
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
