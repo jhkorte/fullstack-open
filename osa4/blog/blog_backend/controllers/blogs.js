41d1dd726254bdca932d4926c305e9191dcc031c
Jimi <jhkorte@proton.me>
2025-08-10 14:50:36 +0300

    4.2 & 4.1

---
+++ osa4/blog/blog_backend/controllers/blogs.js
@@ -0,0 +1,27 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save()
+  .then((result) => {
+    response.status(201).json(result)
+    logger.info('added a new blog', result)
+    Blog.find({}).then(blogs => {
+        logger.info('After adding the new blog:')
+        blogs.forEach(blog => logger.info(blog))
+        logger.info('-----------------------------------------')
+    })
+  })
+})
+
+
+module.exports = blogsRouter
