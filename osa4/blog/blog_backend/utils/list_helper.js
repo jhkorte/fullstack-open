6c1c3349a61c08b4097bc1e167286d1f6484dcb8
Jimi <jhkorte@proton.me>
2025-08-12 19:02:22 +0300

    4.6 & 4.5 & 4.4

--- osa4/blog/blog_backend/utils/list_helper.js
+++ osa4/blog/blog_backend/utils/list_helper.js
@@ -1,7 +1,53 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    return blogs.length === 0
+        ? 0
+        : blogs
+            .map(blog => blog.likes)
+            .reduce(reducer, 0)
+        
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return 0
+
+    const blogLikes = blogs.map(blog => blog.likes)
+    const mostLikesIndex = blogLikes.indexOf(Math.max(...blogLikes))
+
+    return blogs[mostLikesIndex]
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return 0
+
+    const blogAuthors = blogs.map(blog => blog.author)
+
+    // This returns a pair of the name and amount of blogs
+    const [mostBlogsAuthor, blogsAmount] = _
+        .chain(blogAuthors)
+        .countBy()
+        .toPairs()
+        .maxBy(author => author[1])
+        .value()
+
+    return {
+        author: mostBlogsAuthor,
+        blogs: blogsAmount
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
 }
