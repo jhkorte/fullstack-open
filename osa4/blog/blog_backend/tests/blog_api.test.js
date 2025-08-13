67d2dd3bf3b9c7370b35edefccae269888457aeb
Jimi <jhkorte@proton.me>
2025-08-13 21:21:02 +0300

    4.10

--- osa4/blog/blog_backend/tests/blog_api.test.js
+++ osa4/blog/blog_backend/tests/blog_api.test.js
@@ -25,12 +25,29 @@ test('all blogs are returned and in correct JSON format', async () => {
 test('making sure id field is "id", not "_id"  ', async () => {
 	const blogs = await helper.blogsInDatabase()
 
-	console.log(blogs)
-
 	blogs.forEach(blog => {
   	assert(blog.hasOwnProperty('id'));
   	assert(!blog.hasOwnProperty('_id'));
 	})
+})
+
+test('blogs can be added, size increments correctly and new blog title is found', async () => {
+	const blogsAtStart = await helper.blogsInDatabase()
+
+	const newBlog = {
+    title: "This is a new blog",
+    author: "Bloggy McBlogger",
+    url: "https://example.com/",
+    likes: 123,
+	}
+
+	await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+
+	const blogsAtEnd = await helper.blogsInDatabase()
+	assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+
+	const blogTitles = blogsAtEnd.map(b => b.title)
+	assert(blogTitles.includes('This is a new blog'))
 })
 
 
