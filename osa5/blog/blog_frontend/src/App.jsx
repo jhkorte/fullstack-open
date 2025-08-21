89aec5400a8f4c411cba047de8ea518d07b32958
Jimi <jhkorte@proton.me>
2025-08-21 12:52:19 +0300

    lisätty login form frontendiin, ei toiminnallisuutta

--- osa5/blog/blog_frontend/src/App.jsx
+++ osa5/blog/blog_frontend/src/App.jsx
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +13,37 @@ const App = () => {
     )  
   }, [])
 
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log('logging in from frontend with', username, password)
+  }
+
   return (
     <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({target}) => setUsername(target.value)}
+            />
+        </div>
+        <div>
+          password
+            <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({target}) => setPassword(target.value)}
+            />
+        </div>
+        <button type="submit">login</button>
+      </form>
+
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
