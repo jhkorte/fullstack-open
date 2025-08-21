48fd9555852ef4550fb03e6e0072368d8a8a23cf
Jimi <jhkorte@proton.me>
2025-08-21 12:59:14 +0300

    alkeelliset errorit frontendiin

--- osa5/blog/blog_frontend/src/App.jsx
+++ osa5/blog/blog_frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,13 +19,31 @@ const App = () => {
   }, [])
 
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault()
     console.log('logging in from frontend with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('no user found with these credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
+
+  
+
 
   return (
     <div>
+      <Notification message={errorMessage} />
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
         <div>
