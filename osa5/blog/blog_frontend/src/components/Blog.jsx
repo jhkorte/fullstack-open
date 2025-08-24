5bb7aca8a4e0979c47c81904402555a91ae5ecf2
Jimi <jhkorte@proton.me>
2025-08-24 15:50:34 +0300

    lisätty nappi, jolla toggletaan näytetäänkö blogista kaikki info

--- osa5/blog/blog_frontend/src/components/Blog.jsx
+++ osa5/blog/blog_frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showAllInfo, setShowAllInfo] = useState(false)
+
+  const infoMinimum = { display: showAllInfo ? 'none' : '' }
+  const infoAll = { display: showAllInfo ? '' : 'none' }
+
+  const toggleShowAllInfo = () => {
+    setShowAllInfo(!showAllInfo)
+  }
+
+  return (
   <div>
-    {blog.title} - {blog.author} - <button>view</button>
+    <div style={infoMinimum}>
+      {blog.title} - {blog.author} - <button onClick={toggleShowAllInfo}>Show more</button>
+    </div>
+    <div style={infoAll}>
+      {blog.title} <br />
+      {blog.author} <br />
+      {blog.url} <br />
+      {blog.likes} <br /> 
+      <button onClick={toggleShowAllInfo}>Hide</button>
+    </div>
   </div>  
-)
+)}
 
 export default Blog
