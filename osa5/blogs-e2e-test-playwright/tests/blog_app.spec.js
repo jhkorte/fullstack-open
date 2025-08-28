3ba040cb7473628c0dcdd14f876cb6562c951be3
Jimi <jhkorte@proton.me>
2025-08-28 15:30:39 +0300

    aloitettu ensimmäistä testiä

--- osa5/blogs-e2e-test-playwright/tests/blog_app.spec.js
+++ osa5/blogs-e2e-test-playwright/tests/blog_app.spec.js
@@ -4,6 +4,8 @@ test('front page can be opened', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   //TODO
-  const locator = page.getByText('')
+  const locator = page.getByText('The Bloglist')
+  await expect(locator).toBeVisible()
+
 
 })
